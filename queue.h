2328013c13971497ca16603abdb6703a9619b241
Nirja Basawa <113481461+nirjabasawa@users.noreply.github.com>
2024-06-25 16:01:42 -0700

    cleanup, add comments

--- queue.h
+++ queue.h
@@ -1,10 +1,7 @@
-/**
- * @File queue.h
- *
- * The header file that you need to implement for assignment 3.
- *
- * @author Andrew Quinn
- */
+// 
+// Nirja Basawa
+// 05/21/24
+// queue.h
 
 #pragma once
 
@@ -12,49 +9,17 @@
 #include <stdint.h>
 #include <sys/types.h>
 
-/** @struct queue_t
- *
- *  @brief This typedef renames the struct queue.  Your `c` file
- *  should define the variables that you need for your queue.
- */
+// create queue struct type
 typedef struct queue queue_t;
 
-/** @brief Dynamically allocates and initializes a new queue with a
- *         maximum size, size
- *
- *  @param size the maximum size of the queue
- *
- *  @return a pointer to a new queue_t
- */
+// new queue - dynamically allocate and initialize queue with max size 'size'
 queue_t *queue_new(int size);
 
-/** @brief Delete your queue and free all of its memory.
- *
- *  @param q the queue to be deleted.  Note, you should assign the
- *  passed in pointer to NULL when returning (i.e., you should set
- *  *q = NULL after deallocation).
- *
- */
+// delete queue and free memory
 void queue_delete(queue_t **q);
 
-/** @brief push an element onto a queue
- *
- *  @param q the queue to push an element into.
- *
- *  @param elem th element to add to the queue
- *
- *  @return A bool indicating success or failure.  Note, the function
- *          should succeed unless the q parameter is NULL.
- */
+// push element elem onto queue q, return boolean value indicating success or failure
 bool queue_push(queue_t *q, void *elem);
 
-/** @brief pop an element from a queue.
- *
- *  @param q the queue to pop an element from.
- *
- *  @param elem a place to assign the poped element.
- *
- *  @return A bool indicating success or failure.  Note, the function
- *          should succeed unless the q parameter is NULL.
- */
+// pop element elem from queue q, return boolean value indicating success or failure
 bool queue_pop(queue_t *q, void **elem);
